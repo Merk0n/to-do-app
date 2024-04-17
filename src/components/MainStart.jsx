bee32bb55d13dee4e1ec50e5f7de4c1943774685
merk0n <127611734+Merk0n@users.noreply.github.com>
2024-04-17 17:26:16 +0200

    Update MainStart component heading text

--- src/components/MainStart.jsx
+++ src/components/MainStart.jsx
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 const MainStart = ({ onStartAddTask }) => {
   return (
     <div className='m-5 text-center'>
-      <h2>Select a task or start new one</h2>
+      <h2>Select a task or add new one</h2>
 
       <Button onClick={onStartAddTask} variant='outline-secondary'>
         Create new task
